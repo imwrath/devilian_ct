5be445a5624c9f9b74d402b34d2f5c94189bddf2
kondors1995 <normandija1945@gmail.com>
2018-09-22 21:07:53 +0000

    Enable PRODUCT_ENFORCE_RRO_TARGETS for framework-res

--- msm8953.mk
+++ msm8953.mk
@@ -29,6 +29,9 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
     SystemUI \
     framework-res \
 
+PRODUCT_ENFORCE_RRO_TARGETS := \
+    framework-res \
+
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
